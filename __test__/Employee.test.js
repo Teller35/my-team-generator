44e61c0308b9a2f7f95e549c7715be452349cf49
Teller Wetzel <tellerwetzel@yahoo.com>
2021-05-19 11:39:11 -0600

    wrote tests and passing functions for employee.js

---
+++ __test__/Employee.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+test ('create employee object', () => {
+    const employee = new Employee('Teller', '2', 'teller@gmail.com');
+
+    expect(employee.name).toBe('Teller');
+    expect(employee.id).toBe('2');
+    expect(employee.email).toBe('teller@gmail.com');
+})
+
+test ('gets employee name', () => {
+    const employee = new Employee('Teller', '2', 'teller@gmail.com');
+
+    expect(employee.nameInput()).toEqual(expect.stringContaining(employee.name));
+})
+
+test ('gets employee id', () => {
+    const employee = new Employee('Teller', '2', 'teller@gmail.com');
+
+    expect(employee.idInput()).toEqual(expect.stringContaining(employee.id));
+})
+
+test ('gets employee email', () => {
+    const employee = new Employee('Teller', '2', 'teller@gmail.com');
+
+    expect(employee.emailInput()).toEqual(expect.stringContaining(employee.email));
+})
