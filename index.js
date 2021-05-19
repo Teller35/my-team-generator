d953ec916512e2bc99fbe14f75a731afc77c5291
Teller Wetzel <tellerwetzel@yahoo.com>
2021-05-19 10:40:38 -0600

    working questions and loop

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const { inheritInnerComments, validate } = require('@babel/types');
 const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
+const team = [];
 
-function init () {
+
+function makeManager () {
     return inquirer.prompt([
         {
             type: 'input',
@@ -56,6 +63,153 @@ function init () {
             }
         }
     ])
+    .then (() => {
+        add();
+    })
 }
 
-init();
+async function add () {
+    let { employee } = await
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: 'What type of employee would you like to add to team?',
+            choices: ['Engineer', 'Intern', 'None']
+        }
+    ])
+    switch (employee) {
+        case 'Engineer':
+            makeEngineer();
+            break;
+        case 'Intern':
+            makeIntern();
+            break;
+        default:
+            console.log(team);
+            // makeTeam(team);
+    }
+}
+
+function makeEngineer () {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engName',
+            message: 'Please enter engineer name!',
+            validate: engNameInput => {
+                if (engNameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a name!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engId',
+            message: 'Please enter engineer ID number!',
+            validate: engIdInput => {
+                if (engIdInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter an ID number!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engEmail',
+            message: 'Please enter engineer email address!',
+            validate: engEmailInput => {
+                if (engEmailInput) {
+                    return true;
+                }
+                else {
+                    console.log('Need an email address!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter a GitHub user name for engineer!',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Need a user name!');
+                }
+            }
+        }
+    ])
+    .then (() => {
+        add();
+    })
+}
+
+function makeIntern () {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'intName',
+            message: 'Please enter intern name!',
+            validate: intNameInput => {
+                if (intNameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a name!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'intId',
+            message: 'Please enter intern ID number!',
+            validate: intIdInput => {
+                if (intIdInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter an ID number!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'intEmail',
+            message: 'Please enter intern email address!',
+            validate: intEmailInput => {
+                if (intEmailInput) {
+                    return true;
+                }
+                else {
+                    console.log('Need an email address!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter a school name for intern!',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Need a school name!');
+                }
+            }
+        }
+    ])
+    .then (() => {
+        add();
+    })
+}
+
+
+makeManager();
